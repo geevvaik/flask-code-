d0b691b22e658b2944958b4090d41b46d58bccb3
rahimejaz <rahimejaz234@gmai.com>
2022-06-10 01:57:16 +0500

    new update

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ def home():
 
 if __name__ == "__main__":
    app.run()
+
+{% block styles %}
+{{super()}}
+<link rel="stylesheet"
+      href="{{url_for('.static', filename='mystyle.css')}}">
+{% endblock %}
