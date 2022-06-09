5ffb75c4f2a572b1e723bc217039afae807454c2
rahimejaz <rahimejaz234@gmai.com>
2022-06-10 02:38:12 +0500

    new update

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask('server app')
 def home():
     return render_template('home.html')
 
+    
+app = Flask(__name__, static_url_path='/static')
+
 
 if __name__ == "__main__":
    app.run()
+   
 
